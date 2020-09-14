cb4fa23d79f872d82d41c3df69606dab37ebf78f
thomas-lane <70451454+thomas-lane@users.noreply.github.com>
2020-09-14 13:29:15 -0400

    Project upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace TomShare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnDirectory = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblRecipient = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(13, 40);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(371, 160);
+            this.lstFiles.TabIndex = 1;
+            // 
+            // btnDirectory
+            // 
+            this.btnDirectory.Location = new System.Drawing.Point(237, 13);
+            this.btnDirectory.Name = "btnDirectory";
+            this.btnDirectory.Size = new System.Drawing.Size(147, 23);
+            this.btnDirectory.TabIndex = 2;
+            this.btnDirectory.Text = "Choose Receive Directory";
+            this.btnDirectory.UseVisualStyleBackColor = true;
+            this.btnDirectory.Click += new System.EventHandler(this.BtnDirectory_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(13, 13);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.ReadOnly = true;
+            this.txtDirectory.Size = new System.Drawing.Size(218, 20);
+            this.txtDirectory.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(127, 233);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(158, 20);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(291, 233);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(93, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // lblRecipient
+            // 
+            this.lblRecipient.AutoSize = true;
+            this.lblRecipient.Location = new System.Drawing.Point(12, 236);
+            this.lblRecipient.Name = "lblRecipient";
+            this.lblRecipient.Size = new System.Drawing.Size(109, 13);
+            this.lblRecipient.TabIndex = 6;
+            this.lblRecipient.Text = "Recipient IP Address:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AutoSize = true;
+            this.btnAdd.Location = new System.Drawing.Point(12, 204);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.AutoSize = true;
+            this.btnRemove.Location = new System.Drawing.Point(138, 204);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(117, 23);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Location = new System.Drawing.Point(261, 204);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(123, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lblRecipient);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.btnDirectory);
+            this.Controls.Add(this.lstFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TomShare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnDirectory;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblRecipient;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TomShare {
+    public partial class Form1 : Form {
+        private FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+        private OpenFileDialog openFile = new OpenFileDialog();
+        private Thread sendThread;
+
+        public Form1() {
+            InitializeComponent();
+            folderDialog.Description = "Select the directory to receive files in";
+            folderDialog.ShowDialog();
+            txtDirectory.Text = folderDialog.SelectedPath;
+            openFile.Title = "Select file(s) to send";
+            openFile.Multiselect = true;
+            StartListener();
+        }
+
+        private void StartListener() {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 50123);
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(AcceptClient), listener);
+        }
+
+        public void AcceptClient(IAsyncResult ar) {
+            TcpListener listener = (TcpListener) ar.AsyncState;
+
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+
+            //BinaryReader reader = new BinaryReader(client.GetStream());
+            NetworkStream stream = client.GetStream();
+
+            string fileName = "";
+            char readChar = ' ';
+            while((readChar = (char) stream.ReadByte()) != '\n') {
+                fileName += readChar;
+            }
+
+            FileStream file = File.Create(folderDialog.SelectedPath + '\\' + fileName);
+
+            int readByte = 0;
+            while((readByte = stream.ReadByte()) != -1) {
+                file.WriteByte((byte) readByte);
+            }
+
+            file.Flush();
+
+            file.Close();
+
+            client.Close();
+            listener.BeginAcceptTcpClient(new AsyncCallback(AcceptClient), listener);
+            MessageBox.Show("Received file: " + fileName);
+        }
+
+        private void SendFile(string address, string fileName) {
+            TcpClient client = new TcpClient();
+
+            client.Connect(address, 50123);
+
+            string name = fileName;
+
+            int index = name.Length - 1;
+
+            while (name[index] != '\\')  {
+                index--;
+            }
+
+            string nameFormatted = "";
+            for (int i = index + 1; i < name.Length; i++) {
+                nameFormatted += name[i];
+            }
+
+            BinaryWriter writer = new BinaryWriter(client.GetStream());
+            writer.Write(nameFormatted.ToCharArray());
+            writer.Write('\n');
+            writer.Flush();
+
+            FileStream file = File.Open(name, FileMode.Open);
+
+            int read = -1;
+            while ((read = file.ReadByte()) != -1) {
+                writer.Write((byte) read);
+            }
+
+            writer.Flush();
+
+            client.Close();
+            file.Close();
+        }
+
+        private void BtnDirectory_Click(object sender, EventArgs e) {
+            if (folderDialog.ShowDialog() == DialogResult.OK) txtDirectory.Text = folderDialog.SelectedPath;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e) {
+            if(openFile.ShowDialog() == DialogResult.OK) {
+                foreach(string file in openFile.FileNames) {
+                    lstFiles.Items.Add(file);
+                }
+            }
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e) {
+            if(lstFiles.SelectedIndex != -1) lstFiles.Items.RemoveAt(lstFiles.SelectedIndex);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e) {
+            lstFiles.Items.Clear();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e) {
+            if(sendThread != null) {
+                if (sendThread.IsAlive) {
+                    MessageBox.Show("Already sending", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+
+            sendThread = new Thread(SendFiles);
+            sendThread.Start();
+        }
+
+        private void SendFiles() {
+            string[] files = new string[lstFiles.Items.Count];
+            lstFiles.Items.CopyTo(files, 0);
+            foreach(string file in files) {
+                SendFile(txtAddress.Text, file);
+            }
+        }
+    }
+}
